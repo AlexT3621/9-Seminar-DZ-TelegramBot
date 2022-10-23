6a15d8cb51e62cfccf368205e44345999de2b08e
Alex Belyaev <t433621@yandex.ru>
2022-10-23 08:34:32 +0300

    add main

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+# async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+app = ApplicationBuilder().token('TOKEN').build()
+
+app.add_handler(CommandHandler("hello", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+print('start server')
+
+app.run_polling()
